f8de75f6d83d5b08877c484a1e0d96ad0becd709
sirineat <sirineat012@gmail.com>
2023-02-20 12:22:59 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<h1><?php echo "bienvenue sur docker, nous utilisons la php : ". phpversion();?></h1>
+<?php
+$host = 'mysql' ;
+$charset = "utf8";
+$db = 'docker_database';
+$user = 'root';
+$password = '1234';
+$conn = new mysqli($host , $user , $password , $db);
+if($conn->connect_error){
+	echo 'La connexion à mysql a échouée'. $conn->connect_error;
+} else {
+	echo 'La connextion mysql a reussit ! ;)';
+}
+
+$sql = 'SELECT * FROM Persons';
+
+if($result = $conn->query($sql)) {
+	while ($data = $result->fetch_object()) {
+		$Persons[] = $data;
+	}
+}
+
+foreach ($Persons as $person) {
+    echo "<br>";
+    echo $person->FirstName . " " . $person->LastName ;
+    echo "<br>";
+}
+
+?>
+	
